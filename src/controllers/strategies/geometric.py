bd320fb686cb3809c3ed635e4d5375906e3b26cb
jesucrist0 <jesus.milian46632@ucaldas.edu.co>
2026-03-31 16:23:53 -0500

    first_commit

---
+++ src/controllers/strategies/geometric.py
@@ -0,0 +1,14 @@
+class Geometric(SIA):
+    def __init__(self, gestor):
+       super().__init__(gestor)
+       # ...atributos útiles a la estrategia
+    def aplicar_estrategia(self):
+     """
+       Implementa el algoritmo para encontrar la bipartición óptima
+       utilizando el enfoque topológico.
+     """
+     # 1. Construir la representación n-dimensional del sistema
+     # 2. Calcular la tabla de costos (T) para cada variable
+     # 3. Identificar las biparticiones candidatas
+     # 4. Evaluar y seleccionar la bipartición óptima
+     # 5. Retornar el resultado en formato compatible
